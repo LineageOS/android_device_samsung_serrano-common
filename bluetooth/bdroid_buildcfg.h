fe9af6589fc01907e03dacfd46fc1580c19a2aa0
Arne Coucheron <arco68@gmail.com>
2013-11-12 17:59:29 +0100

    Remove BTM_DEF_LOCAL_NAME
     * Will make it use product model instead
    
    Change-Id: I070fa532e7ab0fe32ffdacd69f9ec969004f14b6

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S4 Mini"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
