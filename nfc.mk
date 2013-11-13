1292de1995a6248d3979153cac077e9719fb1ec0
Arne Coucheron <arco68@gmail.com>
2013-11-13 22:47:29 +0100

    Add NFC host-based card emulation feature
    Change-Id: I350b09e0e67adbc0b0a4809ccf9af7257b83800c

--- nfc.mk
+++ nfc.mk
@@ -15,7 +15,8 @@
 # NFC permissions
 PRODUCT_COPY_FILES += \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 # NFC packages
 PRODUCT_PACKAGES += \
