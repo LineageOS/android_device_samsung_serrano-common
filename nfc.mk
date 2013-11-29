a3037292f98aaa07a4b044438acfb221eded856c
Arne Coucheron <arco68@gmail.com>
2013-11-29 14:10:20 +0100

    Update NFC configuration
     * Remove secure element, as it's not supported by device
    
    Change-Id: I07204663a7115bad7c9807e1dcff27ab317f582c

--- nfc.mk
+++ nfc.mk
@@ -12,22 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# NFC permissions
+# NFC access control + feature files + configuration
 PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
 # NFC packages
 PRODUCT_PACKAGES += \
-    libnfc-nci \
-    libnfc_nci_jni \
     nfc_nci.msm8960 \
     NfcNci \
-    Tag \
-    com.android.nfc_extras
-
-# NFCEE access control + configuration
-NFCEE_ACCESS_PATH := device/samsung/serrano-common/nfc/nfcee_access.xml
-PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
+    Tag
