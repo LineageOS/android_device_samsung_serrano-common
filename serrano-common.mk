d0b33ca1426550019c2124036fb1865dd5311245
Arne Coucheron <arco68@gmail.com>
2013-12-16 05:16:09 +0100

    Enable smooth-streaming video-playback
    Change-Id: I0d9572c664021535e9792b4be0071ad016fcfa19

--- serrano-common.mk
+++ serrano-common.mk
@@ -172,7 +172,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vold.umsdirtyratio=50 \
     ro.cwm.enable_key_repeat=true \
     persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0
+    persist.sys.wfd.virtual=0 \
+    mm.enable.smoothstreaming=true
 
 # For userdebug builds
 ADDITIONAL_DEFAULT_PROPERTIES += \
