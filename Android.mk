b53846a2c84f1b5deb87aa371825da4c11a44a1b
Arne Coucheron <arco68@gmail.com>
2014-02-25 01:26:34 +0100

    serrano: Add serranodsdd to build guard
    Change-Id: Ic5cd3b34bb56ce0b6e9454c43481ceafa02a5a1f

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-ifneq ($(filter serrano3gxx serranodsub serranoltexx,$(TARGET_DEVICE)),)
+ifneq ($(filter serrano3gxx serranodsdd serranodsub serranoltexx,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 endif
