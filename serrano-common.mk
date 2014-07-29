97de629f9f2d090241f8a1c61ff6ead9d4a1cf73
Ethan Chen <intervigil@gmail.com>
2014-07-29 14:01:39 -0700

    serrano-common: Clean up Samsung device inheritance
    * Remove init.qcom.class_main and init.qcom.class_core
    * Remove init.qcom.early_boot.sh
    * Remove init.qcom.ril.sh
    * Move init.qcom.syspart_fixup.sh to serrano-common
    * Remove init.qcom.sh
    * Move init.qcom.post_boot.sh to serrano-common
    * Move init.qcom.thermal_conf.sh to serrano-common
    * Move 8930 thermald configs to serrano-common
    * Remove init.qcom.usb.sh
    
    Change-Id: I810d049ca426e07592857efc373eae9a794aa8f2

--- serrano-common.mk
+++ serrano-common.mk
@@ -60,6 +60,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
+    $(LOCAL_PATH)/rootdir/init.qcom.syspart_fixup.sh:root/init.qcom.syspart_fixup.sh \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
@@ -72,7 +73,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
     $(LOCAL_PATH)/rootdir/system/etc/init.qcom.mdm_links.sh:system/etc/init.qcom.mdm_links.sh \
     $(LOCAL_PATH)/rootdir/system/etc/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+    $(LOCAL_PATH)/rootdir/system/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    $(LOCAL_PATH)/rootdir/system/etc/init.qcom.thermal_conf.sh:system/etc/init.qcom.thermal_conf.sh \
     $(LOCAL_PATH)/rootdir/system/etc/init.qcom.wifi.sh:system/etc/init.qcom.wifi.sh
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/system/etc/thermald-8930.conf:system/etc/thermald-8930.conf \
+    $(LOCAL_PATH)/rootdir/system/etc/thermald-8930ab.conf:system/etc/thermald-8930ab.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
