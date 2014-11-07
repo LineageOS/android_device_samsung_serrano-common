184cad2db530833005f273631982dcccf111e619
Arne Coucheron <arco68@gmail.com>
2014-11-07 02:32:49 +0100

    Move power management to a separate init.qcom.power.rc
    Change-Id: I48545a3d843c14a13d609366ce3c50038482f7e6

--- serrano-common.mk
+++ serrano-common.mk
@@ -60,6 +60,7 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.syspart_fixup.sh:root/init.qcom.syspart_fixup.sh \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
