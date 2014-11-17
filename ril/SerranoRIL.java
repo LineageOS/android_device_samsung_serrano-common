86fc5233f2c6f1cafef1fe8b61fd329dd920d1a2
invisiblek <dan.pasanen@gmail.com>
2014-11-18 00:32:36 +0100

    Add overloaded method for new RIL
    Change-Id: I756aa0dcadc89e88bc75274ea1c8229d5d7c97eb

--- ril/SerranoRIL.java
+++ ril/SerranoRIL.java
@@ -43,6 +43,11 @@ public class SerranoRIL extends RIL {
     private static final int RIL_UNSOL_WB_AMR_STATE = 11017;
     private static final int RIL_UNSOL_RESPONSE_HANDOVER = 11021;
 
+    public SerranoRIL(Context context, int preferredNetworkType,
+            int cdmaSubscription, Integer instanceId) {
+        this(context, preferredNetworkType, cdmaSubscription);
+    }
+
     public SerranoRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
         mQANElements = 6;
