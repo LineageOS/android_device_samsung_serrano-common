6f808b167a4f3defc5277c456229fb3a15a62433
Arne Coucheron <arco68@gmail.com>
2015-01-30 06:33:51 +0100

    [WIP] audio: Changes for new audio HAL
     * Deprecate old defines
     * Deprecate old system props and use new ones
     * Add custom audio_platform_info.xml
     * Add custom mixer_paths.xml
     * Change audio_policy.conf for new HAL
     * Remove old sound use case file
    
    Change-Id: I3b180427743f56b384a72f948948a357ae7310d4

--- serrano-common.mk
+++ serrano-common.mk
@@ -31,8 +31,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Media Profile
 PRODUCT_COPY_FILES += \
