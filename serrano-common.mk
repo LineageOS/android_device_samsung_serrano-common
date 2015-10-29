da6591db7d67e5cffcc620b551205292eb33653a
Arne Coucheron <arco68@gmail.com>
2015-10-29 03:02:20 +0100

    device: Enable MIDI support
    Change-Id: I7b21d612f9b1ddb128371acca9fbe3d0a9cb9c7d

--- serrano-common.mk
+++ serrano-common.mk
@@ -31,7 +31,8 @@ TARGET_SCREEN_WIDTH := 540
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
