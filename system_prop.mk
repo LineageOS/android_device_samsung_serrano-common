36ea01e0f7ae1f7f017bf361ca55a842004b7746
Arne Coucheron <arco68@gmail.com>
2015-12-21 22:06:26 +0100

    prop: Remove props used for debugging during bringup
    Change-Id: Ieb3393cd63c608459d97f1e08275007dbb6e9707

--- system_prop.mk
+++ system_prop.mk
@@ -64,10 +64,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false \
     ro.sys.fw.dex2oat_thread_count=4
-
-# For userdebug builds
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.secure=0 \
-    ro.adb.secure=0 \
-    ro.debuggable=1 \
-    persist.service.adb.enable=1
