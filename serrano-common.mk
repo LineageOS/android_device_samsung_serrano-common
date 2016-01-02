cc11c970646c3dad8ec2011988a112d10129589c
Arne Coucheron <arco68@gmail.com>
2016-01-02 10:08:56 +0100

    Revert "Disable ambient display for now"
    This reverts commit c30206afef5c6c200ae35810984c9cf631befa02.
    
    Change-Id: I0ffa0c78deecab5c40040f7b1384a9c267018a88

--- serrano-common.mk
+++ serrano-common.mk
@@ -121,6 +121,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libril_shim
 
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # call common serrano system props
 $(call inherit-product, device/samsung/serrano-common/system_prop.mk)
 
