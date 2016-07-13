d8a7052baa47785c616774f8e2e9acea6246b23a
Arne Coucheron <arco68@gmail.com>
2016-07-13 07:15:58 +0200

    cmhw: Append BOARD_HARDWARE_CLASS
     * We want to pickup cmhw in hardware/samsung as well
    
    Change-Id: I4b96c92268c2ab47d1be12fd075f56204af3db43

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,4 +80,4 @@ COMMON_GLOBAL_CFLAGS += -DDISABLE_ASHMEM_TRACKING
 BOARD_RIL_CLASS := ../../../device/samsung/serrano-common/ril/
 
 # CMHW
-BOARD_HARDWARE_CLASS := device/samsung/serrano-common/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/serrano-common/cmhw
