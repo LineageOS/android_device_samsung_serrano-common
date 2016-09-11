db24d946d3f1f12fd86142268f37bb03efc13e93
Arne Coucheron <arco68@gmail.com>
2016-09-11 06:22:25 +0200

    Initial N bringup
    Change-Id: Ibca7f80a9422e49c5f7201de010bde86280867e9

--- system_prop.mk
+++ system_prop.mk
@@ -2,10 +2,6 @@
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
-# Set read only default composition for USB
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.usb.default.config=mtp
-
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SerranoRIL \
@@ -49,7 +45,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true \
     wifi.interface=wlan0 \
     ro.qualcomm.bt.hci_transport=smd \
-    ro.sdcardfs.enable=true
+    ro.sys.sdcardfs=true
 
 # QC Perf
 PRODUCT_PROPERTY_OVERRIDES += \
