48a151c5ff150d2e0069d0deca2a01d1fadea58d
Arne Coucheron <arco68@gmail.com>
2017-09-04 02:10:00 +0200

    serrano-common: Move include files to msm8930-common
    Change-Id: Ie6c3d37bbdfcec9847d0034f43a6a4f8aaa1e6cd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,8 +27,6 @@
 # Inherit from proprietary vendor
 -include vendor/samsung/serrano-common/BoardConfigVendor.mk
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/serrano-common/include
-
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE := 0x80200000
