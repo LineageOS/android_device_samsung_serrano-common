b57842e4e171ffb9e0e1d130605d44d21ecd7472
Arne Coucheron <arco68@gmail.com>
2017-09-19 00:37:01 +0200

    serrano-common: Switch to source built RIL
    Change-Id: I7cb6ca950feca995656b6f8947e9c5f2790afbad

---
+++ libril/Android.mk
@@ -0,0 +1,40 @@
+# Copyright 2006 The Android Open Source Project
+
+ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp \
+    ril_event.cpp\
+    RilSocket.cpp \
+    RilSapSocket.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libutils \
+    libbinder \
+    libcutils \
+    libhardware_legacy \
+    librilutils \
+
+LOCAL_STATIC_LIBRARIES := \
+    libprotobuf-c-nano-enable_malloc \
+
+ifeq ($(SIM_COUNT), 2)
+    LOCAL_CFLAGS += -DANDROID_MULTI_SIM
+    LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
+endif
+
+LOCAL_C_INCLUDES += external/nanopb-c
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
+
+LOCAL_MODULE:= libril
+LOCAL_CLANG := true
+LOCAL_SANITIZE := integer
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif # BOARD_PROVIDES_LIBRIL
