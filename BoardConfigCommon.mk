d7bdd8b1414a2043117ce45764cb9438ab1da166
Arne Coucheron <arco68@gmail.com>
2017-10-09 07:37:42 +0200

    serrano-common: Use COMMON_PATH variable
    Change-Id: I57083626ffaf4629d13b132cbee1e3b9a08fe3c5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,8 @@
 # definition file).
 #
 
+COMMON_PATH := device/samsung/serrano-common
+
 # Inherit from common msm8930
 -include device/samsung/msm8930-common/BoardConfigCommon.mk
 
@@ -44,7 +46,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Recovery
 LZMA_RAMDISK_TARGETS := recovery
-TARGET_RECOVERY_FSTAB := device/samsung/serrano-common/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/fstab.qcom
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -59,7 +61,7 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/serrano-common/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
 
 # FM
 AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
@@ -86,8 +88,8 @@ BOARD_RIL_CLASS := ../../../device/samsung/serrano-common/ril/
 BOARD_USES_QC_TIME_SERVICES := true
 
 # CMHW
-BOARD_HARDWARE_CLASS += device/samsung/serrano-common/cmhw
+BOARD_HARDWARE_CLASS += $(COMMON_PATH)/cmhw
 
 ifeq ($(WITH_TWRP),true)
--include device/samsung/serrano-common/twrp.mk
+-include $(COMMON_PATH)/twrp.mk
 endif
