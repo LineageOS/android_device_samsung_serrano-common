5a250eecfaa5c0fa59ce3fc5470f2963871d3eb5
Arne Coucheron <arco68@gmail.com>
2017-10-09 07:35:42 +0200

    serrano-common: Set selinux permissive
    Change-Id: If096494171510a3e09b1b608bab37ca2b9be703b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,7 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
