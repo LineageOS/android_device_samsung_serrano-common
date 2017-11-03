ab1fc7889d424266e1c10a6fd5a6bc9b12803e28
Arne Coucheron <arco68@gmail.com>
2017-11-03 06:35:54 +0100

    serrano-common: Use xhdpi-2048 dalvik parameters
    Change-Id: I092197a448b4c9dedbcff5ffd6ec5dcfac7e10c6

--- serrano-common.mk
+++ serrano-common.mk
@@ -154,4 +154,4 @@ $(call inherit-product, device/samsung/serrano-common/system_prop.mk)
 $(call inherit-product, device/samsung/msm8930-common/msm8930.mk)
 
 # call dalvik heap config
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
