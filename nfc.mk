4e052f84ca0da0614526cc82a20dbe669e58a055
Ruchi Kandoi <kandoiruchi@google.com>
2017-12-07 13:36:30 -0800

    serrano-common: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Change nfc.mk as well to reflect the new file name
    
    Test: compiles
    Change-Id: Ic7d88b13cd79705aced6283ca8ee5fb067611941

--- nfc.mk
+++ nfc.mk
@@ -17,7 +17,7 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 # NFC packages
 PRODUCT_PACKAGES += \
