c5cabe7e3f244ebbb0b4fe44706f83536b2fd874
Arne Coucheron <arco68@gmail.com>
2018-02-01 07:45:07 +0100

    serrano-common: Move NFC back to passthrough
    Change-Id: I79667cff4396d83d55f633685cd01c486a8d38eb

--- nfc.mk
+++ nfc.mk
@@ -22,7 +22,6 @@ PRODUCT_COPY_FILES += \
 # NFC packages
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl-bcm \
-    android.hardware.nfc@1.0-service \
     nfc_nci.bcm2079x.default \
     NfcNci \
     Tag
