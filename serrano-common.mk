244252d20014000f1cc4e3b65026267978249f1d
Arne Coucheron <arco68@gmail.com>
2018-02-05 01:27:31 +0100

    serrano-common: Build custom HIDL based power HAL
    Change-Id: I06ae2ce04911a05ae71b3de1c44ea5f0d136c931

--- serrano-common.mk
+++ serrano-common.mk
@@ -127,8 +127,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8960
+    android.hardware.power@1.0-service.serrano
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
