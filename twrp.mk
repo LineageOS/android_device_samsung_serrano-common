8a96f33bc065f600a20e387d778a780e5e4c661a
Arne Coucheron <arco68@gmail.com>
2018-04-18 04:04:12 +0200

    twrp: Don't include TWRP app
    Change-Id: Ifb63e937583cdfe2f042e57d6ccac3c30962650c

--- twrp.mk
+++ twrp.mk
@@ -17,6 +17,7 @@ RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_VARIANT := twrp
 TARGET_RECOVERY_FSTAB := device/samsung/serrano-common/rootdir/twrp.fstab
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXCLUDE_TWRPAPP := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
 TW_NO_LEGACY_PROPS := true
