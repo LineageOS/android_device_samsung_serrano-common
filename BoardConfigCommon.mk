a0bb8a4874b194fc3d634fb04431601bab7a15be
Yifan Hong <elsk@google.com>
2018-04-28 00:04:52 +0200

    serrano-common: healthd: Use vintf_fragments
    This encourages device manufacturers to use
    service.override over service and to remove healthd
    on device, because it is one less step to do.
    
    Test: make VINTF metadata
    Bug: 66917623
    Change-Id: I447395450060cdf17731514bb9a5e3ed912bbc66
    Merged-In: I447395450060cdf17731514bb9a5e3ed912bbc66

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,7 +33,6 @@ COMMON_PATH := device/samsung/serrano-common
 TARGET_SPECIFIC_HEADER_PATH += $(COMMON_PATH)/include
 
 # Manifests
-DEVICE_FRAMEWORK_MANIFEST_FILE := system/libhidl/vintfdata/manifest_healthd_exclude.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Set default locale
