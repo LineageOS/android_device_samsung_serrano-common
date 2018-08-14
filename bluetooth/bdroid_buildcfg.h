7a41f2650378976d811b9758c1f9576459a5376f
Arne Coucheron <arco68@gmail.com>
2018-08-14 13:30:35 +0200

    serrano-common: Disable BLE_VND_INCLUDED for now
    If enabled, crashes the stack when bluetooth is turned on.
    
    Change-Id: I478970f153f8d8d6af44de6e0b21e68b4cb33a37

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -2,7 +2,7 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLE_VND_INCLUDED TRUE
+//#define BLE_VND_INCLUDED TRUE
 #define SC_MODE_INCLUDED FALSE
 #define SDP_DEBUG FALSE
 #define BTSNOOP_MEM FALSE
