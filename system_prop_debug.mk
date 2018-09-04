036d055d879305b9fc8ce106174d721a0f69eccd
Arne Coucheron <arco68@gmail.com>
2018-09-05 01:41:25 +0200

    serrano-common: Fix system_prop_debug.mk
    This stopped working in Oreo. Make the changes so it works again.
    
    Change-Id: Ie77e02d8757c3c5affcd30099a107258bcb82352

--- system_prop_debug.mk
+++ system_prop_debug.mk
@@ -1,5 +1,5 @@
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.secure=0 \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.adb.secure=0 \
     ro.debuggable=1 \
-    persist.service.adb.enable=1
+    persist.service.adb.enable=1 \
+    persist.sys.usb.config=adb
