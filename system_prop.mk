908ef94dacad70ae532cc6ff2d61bf58dc3fbe70
Arne Coucheron <arco68@gmail.com>
2019-08-22 04:59:35 +0200

    serrano-common: Update audiomixers and enable fluence
    So we can have separate handset and voice handset mic
    and use stereo for regular handset mic.
    
    Change-Id: I53b1928310a2a777206af9be01aee6b855726d44

--- system_prop.mk
+++ system_prop.mk
@@ -11,10 +11,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.disable=1 \
+    media.aac_51_output_enabled=true \
     mm.enable.smoothstreaming=true \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    ro.vendor.audio.sdk.fluencetype=fluence \
     vendor.dedicated.device.for.voip=true \
-    vendor.voice.path.for.pcm.voip=true \
-    media.aac_51_output_enabled=true
+    vendor.voice.path.for.pcm.voip=true
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
