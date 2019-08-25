f0211e55b86ec62f09c1df01e8fdf26d16886f59
Arne Coucheron <arco68@gmail.com>
2019-08-26 00:03:59 +0200

    serrano-common: Remove reschedule prop
    Change-Id: I98faa3a4e3b5d45e850c1eef8189c4784ae87e17

--- system_prop.mk
+++ system_prop.mk
@@ -49,8 +49,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-swap=false \
-    ro.vendor.qti.am.reschedule_service=true
+    dalvik.vm.dex2oat-swap=false
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
