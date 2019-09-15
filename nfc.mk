020a411420caa2a63b56ec5f31c12c27c6587e5c
Arne Coucheron <arco68@gmail.com>
2019-09-15 04:54:48 +0200

    Revert "serrano-common: Move NFC HIDL to passthrough, and add manifest from 8930-common"
    This should actually be in the device trees for those variants
    that support NFC, and we'll also want to keep it using hwbinder.
    
    This reverts commit d55e20330a32fa4fe783ecfa4dec252d0481d82d.
    
    Change-Id: Icf16d39a045364742935497f0451fc543c22c364

--- nfc.mk
+++ nfc.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
 # NFC packages
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
     nfc_nci.msm8960 \
     NfcNci \
     Tag
