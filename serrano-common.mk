19f6d583bcba76526ac743c126ef4b7ffe011c9a
Arne Coucheron <arco68@gmail.com>
2019-10-14 08:14:04 +0200

    serrano-common: Copy init.qcom.recovery.rc to recovery/root, not root
    Change-Id: Id7b2a90f714462768a4010eca1c09f63172124b5

--- serrano-common.mk
+++ serrano-common.mk
@@ -70,7 +70,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
-    $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/rootdir/init.qcom.power.rc:recovery/root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
