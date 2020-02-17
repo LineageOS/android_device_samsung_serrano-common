cea4064e2178157563ddb97434cb45f926864c11
Arne Coucheron <arco68@gmail.com>
2020-02-18 00:43:31 +0100

    serrano-common: rootdir: Merge init.target.rc into init.qcom.rc
    Change-Id: Iba8e2471c86502ebf7f7695e3fbc82c7efd6c332

--- serrano-common.mk
+++ serrano-common.mk
@@ -82,7 +82,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.qcom.power.rc:recovery/root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
     $(LOCAL_PATH)/rootdir/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
 # Etc scripts
