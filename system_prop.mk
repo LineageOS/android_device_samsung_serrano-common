9f5f733f831fe2a72a5da99bf103f70cbf7e6b42
Arne Coucheron <arco68@gmail.com>
2020-02-21 01:54:35 +0100

    serrano-common: Enable traced by default
    Change-Id: I6a636cdc59ad0d5d7b598b9415ba2ed6403b0ba7

--- system_prop.mk
+++ system_prop.mk
@@ -52,6 +52,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
 
+# Enable traced by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.traced.enable=1
+
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2016-08-01
