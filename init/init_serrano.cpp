64f434eccadef6e377f1d5b50621eae7b2c44290
Arne Coucheron <arco68@gmail.com>
2020-11-09 03:27:05 +0100

    serrano-common: init: Override system_ext props too
    Change-Id: I4114ab9547dbfea4ba2d043a6be55a9694ce9de3

--- init/init_serrano.cpp
+++ init/init_serrano.cpp
@@ -26,6 +26,7 @@ std::vector<std::string> ro_product_props_default_source_order = {
     "odm.",
     "vendor.",
     "system.",
+    "system_ext.",
 };
 
 void property_override(char const prop[], char const value[], bool add = true)
