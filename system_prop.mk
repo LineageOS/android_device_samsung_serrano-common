f7f440d72f7a68b59e0623efcb4bf75e8a1a1e51
Arne Coucheron <arco68@gmail.com>
2020-12-01 11:09:06 +0100

    serrano-common: Set custom dalvik heap values
    Same as stock ROM.
    
    Change-Id: Ied94bd53650d42f3bfd1f2f1c4b05b770c5f29de

--- system_prop.mk
+++ system_prop.mk
@@ -71,3 +71,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.kill_timeout_ms=100 \
     ro.lmk.log_stats=true \
     ro.lmk.use_minfree_levels=true
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=96m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=1m \
+    dalvik.vm.heapmaxfree=4m
