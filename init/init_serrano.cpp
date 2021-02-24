f39d9e358a6eacedbd39cbfeeacb8ac75e4902b5
Arne Coucheron <arco68@gmail.com>
2021-02-24 05:21:40 +0100

    serrano-common: init: Fix device name in log
    For some reason, ro.product.device returns null when init runs.
    Use ro.product.vendor.device instead, which seems to work fine.
    
    Change-Id: I113356c5ad732a2d87d1b9bcbf4398a13c84be0d

--- init/init_serrano.cpp
+++ init/init_serrano.cpp
@@ -91,6 +91,6 @@ void vendor_load_properties()
         property_override("ro.build.product", "serranoltektt");
     }
 
-    const std::string device = GetProperty("ro.product.device", "");
+    const std::string device = GetProperty("ro.product.vendor.device", "");
     LOG(INFO) << "Found bootloader " << bootloader << ". " << "Setting build properties for " << device << ".\n";
 }
